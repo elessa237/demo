5f20c9154de9fd0e341adf8426ef160e1ddda263
Maxime <elessamaxime@icloud.com>
2021-07-30 14:56:58 +0100

    feat : ajout articlesController

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticlesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function articles(ArticlesRepository $articles): Response
+    {
+        return $this->render('articles/articles.html.twig', [
+            'articles' => $articles->findAll(),
+        ]);
+    }
+}
